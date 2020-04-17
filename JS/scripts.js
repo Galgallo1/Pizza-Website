9a490d6bb15b6473e53c3d474d0e792243109270
Galgallo Tadicha <tadichgalgallo@gmail.com>
2020-04-17 20:34:02 +0300

    add the business logic to the js

---
+++ JS/scripts.js
@@ -0,0 +1,22 @@
+//Business Logic
+
+function Placeorder(type,size,crust,topping,number){
+    this.type=pizzatype;
+    this.size=pizzasize;
+    this.crust=pizzacrust;
+    this.topping=pizzatopping;
+    this.number=pizzanumber;
+};
+
+Placeorder.prototype.summary=function(){
+    return "pizzatype: " + this.type + "size" +this.size + "crust" + this.crust +"topping" + this.topping + "number" + this.number;
+}
+
+//User Interface
+
+$(document).ready(function(){
+
+    $(form#)
+
+
+})
